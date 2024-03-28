7dd9545e66f5d81af534c71d464000ed0281976d
Philippe Moore <mesembria@users.noreply.github.com>
2024-03-28 19:20:12 -0400

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'XXXXX',
+    'password' : 'XXXXX',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'openweather_token' : 'XXXXX',
+    'openweather_location' : 'XXXXX',
+    'lat' : 'XXXXX',
+    'long' : 'XXXXX'
+    }
