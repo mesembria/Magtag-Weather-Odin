259a0b2b18ee17a1495d9d774935141bb3f2b092
Philippe Moore <philippe.moore@gmail.com>
2026-06-27 12:48:16 -0600

    Rename API key field to google_weather_key

--- secrets.py
+++ secrets.py
@@ -2,11 +2,12 @@
 # If you put them in the code you risk committing that info or sharing it
 
 secrets = {
-    'ssid' : 'XXXXX',
-    'password' : 'XXXXX',
+    'ssid' : 'feathers',
+    'password' : 'blackstar',
     'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
-    'openweather_token' : 'XXXXX',
-    'openweather_location' : 'XXXXX',
-    'lat' : 'XXXXX',
-    'long' : 'XXXXX'
+    'google_weather_key': 'YOUR_GOOGLE_WEATHER_KEY_HERE',
+    'openweather_location' : 'Roanoke, US',
+    'lat' : '40.0027362',
+    'long' : '-105.1356593'
+    
     }
